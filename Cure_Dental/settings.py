7a689db29a8bfeaa2e42a50b0037442465732643
abdelrahman <rooted39@gmail.com>
2020-12-16 15:42:31 +0200

    first commit

--- Cure_Dental/settings.py
+++ Cure_Dental/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "f^9%pr!xhs#s&qas!o40v!&et@%a4x^$6iy31gy=rhqar#=g&("
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cure-dental.herokuapp.com']
 
 
 # Application definition
