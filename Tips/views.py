e6cb068ceda1737b1329a9454557758f1621c151
Sourcery AI <>
2023-04-07 19:54:21 +0000

    'Refactored by Sourcery'

--- Tips/views.py
+++ Tips/views.py
@@ -5,6 +5,6 @@ from . import serializers, models
 # Create your views here.
 @api_view(['GET'])
 def tip_view(request):
-    tips = models.Tip.objects.all().order_by("-created_at")[0:1]
+    tips = models.Tip.objects.all().order_by("-created_at")[:1]
     serializer = serializers.TipSerializer(tips, many=True)
     return Response(serializer.data)
